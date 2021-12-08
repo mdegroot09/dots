187a2f98367af664694d6bd571e0b005966c6711
mdegroot09 <mdegroot09@gmail.com>
2021-12-08 12:09:26 -0700

    Added a header

--- src/Components/Home/Home.js
+++ src/Components/Home/Home.js
@@ -1,12 +1,107 @@
-import React, {Component} from 'react';
+import React, { Component } from 'react'
 
-export default class Home extends Component {
+class Home extends Component {
+    constructor(props) {
+        super(props);
+        this.state = { 
+            turnBlue: true
+         }
+    }
 
-    render(){
-        return(
+    render() { 
+        let turn = this.state.turnBlue ? 'Blue' : 'Red'
+        
+        return (
             <div className="home">
-                <h1>Hi</h1>
+                <h1 className="header">Turn: {turn}</h1>
+                <div className="board">
+                    <div className="row">
+                        <div className="dot"></div>
+                        <div className="line horizontalLine"></div>
+                        <div className="dot"></div>
+                        <div className="line horizontalLine"></div>
+                        <div className="dot"></div>
+                        <div className="line horizontalLine"></div>
+                        <div className="dot"></div>
+                        <div className="line horizontalLine"></div>
+                        <div className="dot"></div>
+                    </div>
+                    <div className="row">
+                        <div className="line verticalLine"></div>
+                        <div className="line verticalLine"></div>
+                        <div className="line verticalLine"></div>
+                        <div className="line verticalLine"></div>
+                        <div className="line verticalLine"></div>
+                    </div>
+                    <div className="row">
+                        <div className="dot"></div>
+                        <div className="line horizontalLine"></div>
+                        <div className="dot"></div>
+                        <div className="line horizontalLine"></div>
+                        <div className="dot"></div>
+                        <div className="line horizontalLine"></div>
+                        <div className="dot"></div>
+                        <div className="line horizontalLine"></div>
+                        <div className="dot"></div>
+                    </div>
+                    <div className="row">
+                        <div className="line verticalLine"></div>
+                        <div className="line verticalLine"></div>
+                        <div className="line verticalLine"></div>
+                        <div className="line verticalLine"></div>
+                        <div className="line verticalLine"></div>
+                    </div>
+                    <div className="row">
+                        <div className="dot"></div>
+                        <div className="line horizontalLine"></div>
+                        <div className="dot"></div>
+                        <div className="line horizontalLine"></div>
+                        <div className="dot"></div>
+                        <div className="line horizontalLine"></div>
+                        <div className="dot"></div>
+                        <div className="line horizontalLine"></div>
+                        <div className="dot"></div>
+                    </div>
+                    <div className="row">
+                        <div className="line verticalLine"></div>
+                        <div className="line verticalLine"></div>
+                        <div className="line verticalLine"></div>
+                        <div className="line verticalLine"></div>
+                        <div className="line verticalLine"></div>
+                    </div>
+                    <div className="row">
+                        <div className="dot"></div>
+                        <div className="line horizontalLine"></div>
+                        <div className="dot"></div>
+                        <div className="line horizontalLine"></div>
+                        <div className="dot"></div>
+                        <div className="line horizontalLine"></div>
+                        <div className="dot"></div>
+                        <div className="line horizontalLine"></div>
+                        <div className="dot"></div>
+                    </div>
+                    <div className="row">
+                        <div className="line verticalLine"></div>
+                        <div className="line verticalLine"></div>
+                        <div className="line verticalLine"></div>
+                        <div className="line verticalLine"></div>
+                        <div className="line verticalLine"></div>
+                    </div>
+                    <div className="row">
+                        <div className="dot"></div>
+                        <div className="line horizontalLine"></div>
+                        <div className="dot"></div>
+                        <div className="line horizontalLine"></div>
+                        <div className="dot"></div>
+                        <div className="line horizontalLine"></div>
+                        <div className="dot"></div>
+                        <div className="line horizontalLine"></div>
+                        <div className="dot"></div>
+                    </div>
+                </div>
             </div>
-        )
+        );
     }
 }
+ 
+export default Home;
