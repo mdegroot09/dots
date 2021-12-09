781513f374227a851a8b754b469f795346314965
mdegroot09 <mdegroot09@gmail.com>
2021-12-09 12:24:54 -0700

    Added reset of line clicks

--- src/Components/Home/Home.js
+++ src/Components/Home/Home.js
@@ -36,6 +36,7 @@ class Home extends Component {
 
         Array.prototype.forEach.call(lines, (e) => {
             e.style.background = 'none'
+            e.style.pointerEvents = 'auto'
         })
         Array.prototype.forEach.call(squares, (e) => {
             e.style.background = 'none'
